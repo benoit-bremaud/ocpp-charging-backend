955af8a90ef73e7e8a6a898d3191eb4cb21d5a17
benoit-bremaud <bbd.concept@gmail.com>
2025-12-05 15:40:27 +0100

    feat(app): register FindAllChargePoints use-case in DI
    CHANGES:
    - Add FindAllChargePoints to providers + exports
    
    ARCHITECTURE:
    ✅ Use-case injectable across presentation/integration layers

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CHARGE_POINT_REPOSITORY_TOKEN } from './infrastructure/tokens';
 // Application
 import { SelectChargePoint } from './application/use-cases/SelectChargePoint';
 import { CreateChargePoint } from './application/use-cases/CreateChargePoint';
+import { FindAllChargePoints } from './application/use-cases/FindAllChargePoints';
 
 // Presentation
 import { ChargePointController } from './presentation/controllers/ChargePointController';
@@ -40,7 +41,13 @@ import { ChargePointController } from './presentation/controllers/ChargePointCon
     },
     SelectChargePoint,
     CreateChargePoint,
+    FindAllChargePoints,
   ],
-  exports: [CHARGE_POINT_REPOSITORY_TOKEN, SelectChargePoint, CreateChargePoint],
+  exports: [
+    CHARGE_POINT_REPOSITORY_TOKEN,
+    SelectChargePoint,
+    CreateChargePoint,
+    FindAllChargePoints,
+  ],
 })
 export class AppModule {}
